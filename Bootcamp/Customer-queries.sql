f70083864aba621efcfaf381a46257d93d09955a
Jonelle <jonelle@starberry.us>
2019-04-16 16:45:53 -0400

    initial commit

---
+++ Bootcamp/Customer-queries.sql
@@ -0,0 +1,12 @@
+SELECT * 
+	FROM customer
+   where sales > 30000000 
+   and sales < 40000000
+   order by sales desc;
+    
+    Select name as 'Customer Name', sales as 'Total Sales' 
+    from customer;
+    
+    
+    
+
