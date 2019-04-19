df5354cd90c05ec40c2ccd30d8ed1de56ee6660d
Jonelle <jonelle@starberry.us>
2019-04-18 23:02:27 -0400

    updatedmovie

--- BMDB/Bmdbcreatetables.sql
+++ BMDB/Bmdbcreatetables.sql
@@ -29,7 +29,8 @@ INSERT INTO Genre VALUES
 (19, 'Speculative'),
 (20, 'Thriller'),
 (21, 'Urban'),
-(22, 'Western');
+(22, 'Western'),
+(23, 'Musical');
 
 CREATE TABLE actor (
 	ID				INT            PRIMARY KEY  NOT NULL  AUTO_INCREMENT,
@@ -60,4 +61,67 @@ CREATE TABLE Movie (
     director		varchar(50)
 );
 
-Select * from actor
+INSERT INTO Movie VALUES
+(1, 'Singin'' In the Rain', 'G', '1952', 'Kelly'),
+(2, 'Fiddler on the Roof', 'G', '1971', 'Jewison'),
+(3, 'Hello Dolly', 'G', '1969', 'Kelly'),
+(4, 'My Fair Lady', 'G', '1964', 'Cukor'),
+(5, 'The Sound of Music', 'G', '1965', 'Wise');
+
+CREATE TABLE Credit (
+	ID				INT            PRIMARY KEY  NOT NULL  AUTO_INCREMENT,
+	MovieID			INT,
+    ActorID			INT,
+    roll			varchar(100)				not null,
+FOREIGN KEY (movieID) REFERENCES Movie(ID),
+FOREIGN KEY (ActorID) REFERENCES Actor(ID)
+);
+
+INSERT INTO Credit VALUES 
+(1,1,3,'Don Lockwood'),
+(2,1,5,'Cosmo Brown'),
+(3,1,8,'Kathy Selden'),
+(4,2,7,'Tevye'),
+(5,3,6,'Dolly Levy'),
+(6,4,1, 'Eliza Doolittle'),
+(7,4,4,'Audrey Hepburn''s Voice'),
+(8,5,2,'Maria')
+;
+
+CREATE TABLE MovieGenre (
+	ID				INT            PRIMARY KEY  NOT NULL  AUTO_INCREMENT,
+	MovieID			INT,
+    GenreID			INT,
+FOREIGN KEY (movieID) REFERENCES Movie(ID),
+FOREIGN KEY (GenreID) REFERENCES Genre(ID)
+);
+
+INSERT INTO MovieGenre VALUES 
+(1,1,2),
+(2,1,7),
+(3,1,13),
+(4,1,23),
+(5,2,4),
+(6,2,6),
+(7,2,13),
+(8,2,23),
+(9,3,2),
+(10,3,7),
+(11,3,14),
+(12,4,4),
+(13,4,18),
+(14,4,23),
+(15,5,1),
+(16,5,6),
+(17,5,23);
+
+select actor.firstname, actor.lastname, movie.title, 
+credit.roll
+ from movie
+join moviegenre on movie.id = moviegenre.movieID
+join genre on moviegenre.genreID = genre.ID
+join credit on movie.id = credit.movieID
+join actor on credit.actorID = actor.ID
+where genre.name = 'musical'
+and movie.year >= 1960;
+
